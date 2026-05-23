9dd0a002433e5efb8255b9e7e9910941445b9f40
hezaepark <hezae.park@gmail.com>
2026-05-23 21:38:29 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'lux-compass-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/i18n.js',
+  '/js/calc.js',
+  '/js/app.js',
+  '/manifest.json',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(r => r || fetch(e.request))
+      .catch(() => caches.match('/index.html'))
+  );
+});
